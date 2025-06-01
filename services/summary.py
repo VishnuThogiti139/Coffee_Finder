6ef9d2b424c26d87df5c60eafa353598821e99b6
VishnuThogiti139 <thogitivishnu1079@gmail.com>
2025-05-31 23:05:26 -0400

    Update summary.py

--- services/summary.py
+++ services/summary.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
-    google_api_key=st.secrets("GEMINI_API_KEY"),
+    google_api_key=st.secrets.get("GEMINI_API_KEY"),
     temperature=0.7
 )
 
